1c20705f173d9bcd974dbb8ee83de0a2bfadb11e
heeyonhh <lightram23@gmail.com>
2023-07-09 15:35:59 +0900

    3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+let todoItemId = 0;
+
 //to do item 입력 필드 컴포넌트
 //유저가 어떤 값을 입력하는지 알아야 하니 usestate
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
-  //유저가 usestate입력한 값을 input에 저장하겠다.
-  // console.log(input);
 
   const onSubmit = () => {
     props.onSubmit(input);
@@ -29,10 +29,23 @@ const TodoItemInputField = (props) => {
   </div>);
 }
 
-//todolist 컴포넌트 / array 형태로 리스트 받기 -> itemlist
+const TodoItem = (props) => {
+  //finish가 맞으면 선을 긋기 (클릭했을때 온클릭 이벤트일때)
+  const style = props.todoItem.isFinished ? { textDecoration: 'line-through' } : {};
+  return (<li>
+    <span style={style} onClick={() => props.onTodoItemClick(props.todoItem)}>{props.todoItem.todoItemContent}</span>
+    <Button variant="outlined" onClick={() => props.onRemoveClick(props.todoItem)}>Remove</Button>
+  </li>);
+};
+
 const TodoItemList = (props) => {
   const todoList = props.todoItemList.map((todoItem, index) => {
-    return <li key={index}>{todoItem.todoItemContent}</li>
+  return <TodoItem
+      key={index}
+      todoItem={todoItem}
+      onTodoItemClick={props.onTodoItemClick}
+      onRemoveClick={props.onRemoveClick}
+    />;
   });
 
   return (<div>
@@ -41,12 +54,48 @@ const TodoItemList = (props) => {
 };
 
 function App() {
+  const [todoItemList, setTodoItemList] = useState([]);
+
+  const onSubmit = (newTodoItem) => {
+    setTodoItemList([...todoItemList, {
+      id: todoItemId++,
+      todoItemContent: newTodoItem,
+      isFinished: false,
+    }]);
+  };
+
+  const onTodoItemClick = (clickedTodoItem) => {
+    setTodoItemList(todoItemList.map((todoItem) => {
+      if (clickedTodoItem.id === todoItem.id) {
+        return {
+          id: clickedTodoItem.id,
+          todoItemContent: clickedTodoItem.todoItemContent,
+          isFinished: !clickedTodoItem.isFinished,
+        };
+        //클릭하고 id 만났을 때 피니쉬필드만 반대로 바꿔줘라
+      } else {
+        return todoItem;
+        //나머지 애들은 그대로 킵하고 
+      }
+    }));
+  };
+
+  const onRemoveClick = (removedTodoItem) => {
+    setTodoItemList(todoItemList.filter((todoItem) => {
+      return todoItem.id !== removedTodoItem.id;
+    }));
+  };
+    
   return (
     <div className="App">
-      <TodoItemInputField onSubmit={() => {}}/>
-      <TodoItemList todoItemList={[{
-        todoItemContent: "오늘 할일"
-      }]} />
+      <TodoItemInputField onSubmit={onSubmit} />
+      <TodoItemList
+        todoItemList={todoItemList}
+        onTodoItemClick={onTodoItemClick}
+        //리스트 클릭 스트라이프
+        onRemoveClick={onRemoveClick}
+      />
+
     </div>
   );
 }
